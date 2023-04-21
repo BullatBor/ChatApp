f7cde0bcf41b7b32d4e9d30f13dbef9297160dd0
Bur_Bull <Borboev21@gmail.com>
2023-04-21 17:44:25 +0700

    21.04.23

--- folder1/src/components/Content/MyPosts/Post/Post.jsx
+++ folder1/src/components/Content/MyPosts/Post/Post.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './Post.module.css'
 
 export const Post= ({message}) => {
+  const [like, setLike] = useState(0)
   return (
         <div className={classes.item}>
           <img src='https://e7.pngegg.com/pngimages/84/165/png-clipart-united-states-avatar-organization-information-user-avatar-service-computer-wallpaper-thumbnail.png'/>
           {message}
-          <div>
-            <span>like</span>
+          <div style={{cursor:"pointer"}} onClick={() => setLike(like+1)}>
+            <span>{like} like</span>
           </div>
         </div>        
       
